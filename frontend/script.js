86fb70e26a6956877e1e4cd29c1da9ae39f49f4e
Vopender Chaudhary <vopenderchaudhary@gmail.com>
2025-10-12 21:55:11 +0530

    server.js changes

--- frontend/script.js
+++ frontend/script.js
@@ -211,8 +211,8 @@
   Array.from(this.elements).forEach(el => el.disabled = true);
 
   try {
-    const res = await fetch('https://portfolio2025-lac-delta.vercel.app/api/contact', {
-    // const res = await fetch('http://localhost:5000/api/contact', {
+     const res = await fetch('https://portfolio2025-lac-delta.vercel.app/api/contact', {
+    //const res = await fetch('http://localhost:5000/api/contact', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
